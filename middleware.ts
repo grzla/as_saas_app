cd618b11d005a339a00651a3e2467b31ffe9f2ac
grzla <greg.j.lundberg@gmail.com>
2024-06-30 04:17:46 -0400

    middleware udpate

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
 // beware that authmiddleware, as instructed in the video, is deprecated.
 
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({});
+export default authMiddleware({});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
